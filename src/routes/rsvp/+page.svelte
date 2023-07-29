4864090fa6457a93978d41281707663135bef580
Cade Gray <cadegrayweb@gmail.com>
2023-07-29 15:18:23 -0400

    Updated RSVP page to use svelte bind

--- src/routes/rsvp/+page.svelte
+++ src/routes/rsvp/+page.svelte
@@ -1,25 +1,16 @@
 <script>
 	import HomeLink from '../../homeLink.svelte';
 	var submitted = false;
-	var submitterFirstName = '';
-	var attendingYNValue = 'Y';
+	var firstNameValue = '';
+	var lastNameValue = '';
+	var emailValue = '';
+	var attendingYNValue = 'N';
 	var dietRestValue = null;
-	var p1YNValue = null;
+	var p1YNValue = 'N';
 	var p1NameValue = null;
 	var childrenYNValue = null;
 	var childCountValue = 0;
 	function submitForm() {
-		// Get the input field values
-		var firstNameValue = document.getElementById('first-name').value.trim();
-		var lastNameValue = document.getElementById('last-name').value.trim();
-		var emailValue = document.getElementById('email').value.trim();
-		if (attendingYNValue === 'Y') {
-			dietRestValue = document.getElementById('diet-rest').value;
-			p1YNValue = document.getElementById('plus-one').value;
-			p1NameValue = document.getElementById('p1-name').value;
-			childrenYNValue = document.getElementById('children').value;
-			childCountValue = document.getElementById('child-count').value;
-		}
 		if (
 			firstNameValue === '' ||
 			lastNameValue === '' ||
@@ -29,7 +20,7 @@
 			childCountValue < 0
 		) {
 			alert(
-				'Please fill in all the required fields (First Name, Last Name, Email, or amount of children if you are attending.).'
+				'Please answer all the required questions (First Name, Last Name, Email and if you are attending then also plus one y/n, their name, children y/n, and count of children.).'
 			);
 		} else {
 			// Submit the form or perform any other action here
@@ -59,11 +50,13 @@
 				.then((response) => response.json())
 				.then((data) => {
 					submitted = true;
-					submitterFirstName = formData.firstName;
 				})
 				.catch((error) => {
 					// Handle any errors that occurred during the request
-					alert('There was an error submitting, please contact Cade and Katie.');
+					alert(
+						'There was an error submitting, please contact Cade and Katie and send them this error: ' +
+							error
+					);
 				});
 		}
 		// Create an object to store the data
@@ -75,11 +68,11 @@
 		{#if submitted == false}
 			<h1>RSVP</h1>
 			<label for="first-name"><span class="required">*</span>First Name</label>
-			<input type="text" name="first-name" id="first-name" required />
+			<input type="text" name="first-name" id="first-name" bind:value={firstNameValue} required />
 			<label for="last-name"><span class="required">*</span>Last Name</label>
-			<input type="text" name="last-name" id="last-name" required />
+			<input type="text" name="last-name" id="last-name" bind:value={lastNameValue} required />
 			<label for="email"><span class="required">*</span>Email</label>
-			<input type="email" name="email" id="email" required />
+			<input type="email" name="email" id="email" bind:value={emailValue} required />
 			<label for="attending"><span class="required">*</span>Will you be attending?</label>
 			<div>
 				<div>
@@ -87,21 +80,21 @@
 						type="radio"
 						name="attending"
 						id="attending"
-						value="Y"
+						value="N"
 						bind:group={attendingYNValue}
 						checked
 					/>
-					<label for="attending">Yes</label>
+					<label for="attending">No</label>
 				</div>
 				<div>
 					<input
 						type="radio"
 						name="attending"
 						id="attending"
-						value="N"
+						value="Y"
 						bind:group={attendingYNValue}
 					/>
-					<label for="attending">No</label>
+					<label for="attending">Yes</label>
 				</div>
 			</div>
 			{#if attendingYNValue == 'N'}
@@ -109,45 +102,65 @@
 				<button on:click={submitForm} class="submitButt">Submit</button>
 			{:else}
 				<label for="diet-rest">Any Dietary Restrictions?</label>
-				<input type="text" name="diet-rest" id="diet-rest" />
+				<input type="text" name="diet-rest" id="diet-rest" bind:value={dietRestValue} />
 				<label for="plus-one"><span class="required">*</span>Plus one?</label>
 				<div>
 					<div>
-						<input type="radio" name="plus-one" id="plus-one" value="Y" checked />
-						<label for="plus-one">Yes</label>
+						<input
+							type="radio"
+							name="plus-one"
+							id="plus-one"
+							value="N"
+							bind:group={p1YNValue}
+							checked
+						/>
+						<label for="plus-one">No</label>
 					</div>
 					<div>
-						<input type="radio" name="plus-one" id="plus-one" value="N" />
-						<label for="plus-one">No</label>
+						<input type="radio" name="plus-one" id="plus-one" value="Y" bind:group={p1YNValue} />
+						<label for="plus-one">Yes</label>
 					</div>
 				</div>
 				<label for="p1-name">If plus one, their first and last name:</label>
-				<input type="text" name="p1-name" id="p1-name" />
+				<input type="text" name="p1-name" id="p1-name" bind:value={p1NameValue} />
 				<label for="children"><span class="required">*</span>Bringing Children?</label>
 				<div>
 					<div>
-						<input type="radio" name="children" id="children" value="N" checked />
+						<input
+							type="radio"
+							name="children"
+							id="children"
+							value="N"
+							bind:group={childrenYNValue}
+							checked
+						/>
 						<label for="children">No</label>
 					</div>
 					<div>
-						<input type="radio" name="children" id="children" value="Y" />
+						<input
+							type="radio"
+							name="children"
+							id="children"
+							value="Y"
+							bind:group={childrenYNValue}
+						/>
 						<label for="children">Yes</label>
 					</div>
 				</div>
 				<label for="children"><span class="required">*</span>If bringing children, how many?</label>
-				<input type="number" name="child-count" id="child-count" value="0" />
+				<input type="number" name="child-count" id="child-count" bind:value={childCountValue} />
 				<br />
 				<button on:click={submitForm} class="submitButt">Submit</button>
 			{/if}
 		{:else}
 			{#if attendingYNValue === 'Y'}
 				<h2 class="thanksText">
-					Thank you for RSVPing {submitterFirstName}! We look forward to seeing you on October 28th,
+					Thank you for RSVPing {firstNameValue}! We look forward to seeing you on October 28th,
 					2023! Please visit the FAQ page for additonal information
 				</h2>
 			{:else if attendingYNValue === 'N'}
 				<h2 class="thanksText">
-					Thank you for RSVPing {submitterFirstName}! We wish you could make it but appreciate you
+					Thank you for RSVPing {firstNameValue}! We wish you could make it but appreciate you
 					taking the time to let us know.
 				</h2>
 			{/if}
